a5ad80e5540dbd4114cbaa6e2bd4b71b5ac3fb29
naveenrajgoel <98223311+naveenrajgoel@users.noreply.github.com>
2024-05-07 07:11:10 +0000

    new

--- project01/index.js
+++ project01/index.js
@@ -3,16 +3,28 @@ const app = express();
 const users = require("./MOCK_DATA.json");
 
 const PORT = 8000;
+app.use(express.urlencoded({extended:false}));
 //routes
 app.get('/api/users', (req,res)=> {
-    return res.json(users);
-});
+     return res.json(users);
+ });
 
-app.get('/api/users/:id', (req,res)=>{
+app.route("/api/users/:id")
+    .get
+    ((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user) => user.id===id)
     return res.json(user);
-});
+   })
+   .patch((req,res)=>{
+    return res.json({status: "pending"})
+   })
+   .delete((req,res)=>{
+    return res.json({status: "pending"})
+   });
+
+
+
 
 app.get('/users',(req,res)=>{
     const html = `
@@ -21,6 +33,12 @@ app.get('/users',(req,res)=>{
     </ul>
     `;
     res.send(html);
+});
+
+app.post("/api/users",(req,res)=>{
+    const body = req.body;
+    console.log("Body", body)
+    return res.json({status: "pending"})
 });
 
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`))
